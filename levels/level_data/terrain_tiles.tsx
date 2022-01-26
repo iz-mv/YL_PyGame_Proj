0b163c84df545ad0acd0054ff588f1009002bd93
Islam <islam.mubarakov2020@gmail.com>
2022-01-27 01:31:10 +0300

    c

---
+++ levels/level_data/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrain_tiles" tilewidth="64" tileheight="64" tilecount="400" columns="20">
+ <image source="../../graphics/terrain/terrain_tiles.jpg" width="1300" height="1298"/>
+</tileset>
